4c14bebe53c51964c1dd167b9ef47a90fde626a7
Admin <Admin@DESKTOP-TUKUL5M>
2024-07-04 15:42:24 +0530

    Add project files.

---
+++ Controllers/CreditCardControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using CreditCardValidationAPI.Controllers;
+using CreditCardValidationAPI.Models;
+using CreditCardValidationAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CreditCardValidationTests
+{
+    public class CreditCardControllerTests
+    {
+        private readonly Mock<CreditCardService> _mockService;
+        private readonly CreditCardController _controller;
+
+        public CreditCardControllerTests()
+        {
+            _mockService = new Mock<CreditCardService>();
+            _controller = new CreditCardController(_mockService.Object);
+        }
+
+        [Fact]
+        public void ValidateCreditCard_ShouldReturnBadRequest_WhenCardNumberIsNull()
+        {
+            // Arrange
+            var creditCard = new CreditCard { CardNumber = null };
+
+            // Act
+            var result = _controller.ValidateCreditCard(creditCard);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Credit card number is required.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void ValidateCreditCard_ShouldReturnBadRequest_WhenCardNumberIsEmpty()
+        {
+            // Arrange
+            var creditCard = new CreditCard { CardNumber = "" };
+
+            // Act
+            var result = _controller.ValidateCreditCard(creditCard);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Credit card number is required.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void ValidateCreditCard_ShouldReturnBadRequest_WhenCardNumberIsInvalid()
+        {
+            // Arrange
+            var creditCard = new CreditCard { CardNumber = "abc123" };
+            _mockService.Setup(s => s.ValidateLuhn(It.IsAny<string>())).Throws(new ArgumentException("Card number must contain only digits."));
+
+            // Act
+            var result = _controller.ValidateCreditCard(creditCard);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Card number must contain only digits.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void ValidateCreditCard_ShouldReturnOk_WhenCardNumberIsValid()
+        {
+            // Arrange
+            var creditCard = new CreditCard { CardNumber = "4532015112830366" };
+            _mockService.Setup(s => s.ValidateLuhn(creditCard.CardNumber)).Returns(true);
+
+            // Act
+            var result = _controller.ValidateCreditCard(creditCard);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<CreditCardValidationResult>(okResult.Value);
+            Assert.True(response.IsValid);
+        }
+
+        [Fact]
+        public void ValidateCreditCard_ShouldReturnInternalServerError_OnException()
+        {
+            // Arrange
+            var creditCard = new CreditCard { CardNumber = "4532015112830366" };
+            _mockService.Setup(s => s.ValidateLuhn(It.IsAny<string>())).Throws(new Exception("Unexpected error"));
+
+            // Act
+            var result = _controller.ValidateCreditCard(creditCard);
+
+            // Assert
+            var internalServerErrorResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(500, internalServerErrorResult.StatusCode);
+            Assert.Equal("An unexpected error occurred.", internalServerErrorResult.Value);
+        }
+    }
+}
---
+++ Services/CreditCardServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using CreditCardValidationAPI.Services;
+using System;
+using Xunit;
+
+namespace CreditCardValidationTests
+{
+    public class CreditCardServiceTests
+    {
+        private readonly CreditCardService _service;
+
+        public CreditCardServiceTests()
+        {
+            _service = new CreditCardService();
+        }
+
+        [Theory]
+        [InlineData("4532015112830366", true)] // Valid Visa card.
+        [InlineData("6011514433546201", true)] // Valid Discover card
+        [InlineData("1234567890123456", false)] // Invalid card
+        public void ValidateLuhn_ShouldValidateCorrectly(string cardNumber, bool expected)
+        {
+            var result = _service.ValidateLuhn(cardNumber);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ValidateLuhn_ShouldThrowExceptionForNullOrEmptyInput()
+        {
+            Assert.Throws<ArgumentException>(() => _service.ValidateLuhn(null));
+            Assert.Throws<ArgumentException>(() => _service.ValidateLuhn(""));
+        }
+
+        [Fact]
+        public void ValidateLuhn_ShouldThrowExceptionForNonDigitInput()
+        {
+            Assert.Throws<ArgumentException>(() => _service.ValidateLuhn("abc123"));
+        }
+    }
+}
